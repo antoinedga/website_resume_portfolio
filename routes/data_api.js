e52ef1988a8878d80f37559234299635089284c5
Antoine <an.gordonalvarez@gmail.com>
2020-01-11 23:31:20 -0500

    trying cors for data api

--- routes/data_api.js
+++ routes/data_api.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const quo = require('./models/quotes_model.js');
 var cors = require('cors');
 
-router.route('/').get((req,res) => {
+router.route('/').get(cors(), (req,res) => {
   quo.find()
     .then(list => {
         res.json(list)
