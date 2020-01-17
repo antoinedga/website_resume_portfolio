2d26d5e5e91e3d5cc6a6e8239d96bf9edf53db24
Antoine <an.gordonalvarez@gmail.com>
2020-01-16 21:36:04 -0500

    first phase of making the website more mobile friendly

---
+++ public/Backend_Post/frontend_toBackend.js
@@ -0,0 +1,14 @@
+$("#my_form").submit(function(event){
+	event.preventDefault(); //prevent default action 
+	var post_url = $(this).attr("action"); //get form action url
+	var request_method = $(this).attr("method"); //get form GET/POST method
+	var form_data = $(this).serialize(); //Encode form elements for submission
+	
+	$.ajax({
+		url : post_url,
+		type: POST,
+		data : form_data
+	}).done(function(response){ //
+		$("#server-results").html(response);
+	});
+});
