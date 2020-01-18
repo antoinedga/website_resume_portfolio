c9ca4d82fe978bfce838fbc9bec4734e37b7d5ca
Antoine <an.gordonalvarez@gmail.com>
2020-01-18 23:33:20 +0000

    added validator for backend to add projects

--- routes/project_api.js
+++ routes/project_api.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const proj = require('./models/projects_model');
-var cors = require('cors');
+const Validator = require("validator");
+const isEmpty = require("is-empty");
 
 router.route('/').get((req,res) => {
     proj.find()
@@ -14,16 +15,15 @@ router.route('/').get((req,res) => {
 
   router.route('/add_project').post((req,res) => {
     
-    const name = req.body.name || null;
-    const semester = req.body.semester || null;
-    const description = req.body.description
+    const name = !isEmpty(req.body.name) ? req.body.name : "";
+    const semester = !isEmpty(req.body.semester) ? req.body.name : "";
+    const description = req.body.description;
     const tech = req.body.tech;
     const temp = new proj({name,tech,description,semester});
-  
-
-    if (temp.name == "" || tech.length == 0 || description == 0 || semester == "")
-      return res.status(400).json('error in adding');
     
+    if (Validator.isEmpty(req.body.name) || Validator.isEmpty(req.body.semester))
+      return res.status(400).json("did not add project");
+ 
     temp.save().then(() => res.json(temp).status(200))
     .catch(err => res.status(400).json('Error: ' + err));
   });
