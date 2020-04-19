6a02144ebae2a7623c3047488f738ab844be3671
Antoine <an.gordonalvarez@gmail.com>
2020-04-19 12:29:04 -0400

    ficed mongodb sign in

--- app.js
+++ app.js
@@ -23,7 +23,6 @@ app.use(
   );
   
 app.use(bodyParser.json());
-app.use(helmet());
 app.use(cors());
 
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true } ).
--- routes/keys.js
+++ routes/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-	mongoURI: "mongodb+srv://web_dev:adga761995@cluster0-1rzww.mongodb.net/resumes?retryWrites=true&w=majority",
+	mongoURI: "mongodb+srv://web_dev:zWSno7qJ3G56Jjgb@cluster0-1rzww.mongodb.net/resumes?retryWrites=true&w=majority" ,
 	secretOrKey: "secret"
 };
